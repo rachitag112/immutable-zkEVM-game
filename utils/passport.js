eda6a69dd84db56bf3753fa382ff0d0480bb9d7a
Rachit Agarwal <rachitag112@gmail.com>
2024-01-04 19:53:15 +0530

    Updated passport configuration

--- utils/passport.js
+++ utils/passport.js
@@ -3,8 +3,8 @@ window.passport = new window.immutable.passport.Passport({
       environment: window.immutable.config.Environment.SANDBOX,
     }),
     clientId: 'Npk9mKF4amGIica03M4h9wIydh2iL6A1',
-    redirectUri: 'https://good-quick-jennet.ngrok-free.app',
-    logoutRedirectUri: 'https://good-quick-jennet.ngrok-free.app/logout.html',
+    redirectUri: 'https://immutable-zkevm-game.vercel.app',
+    logoutRedirectUri: 'https://immutable-zkevm-game.vercel.app/logout.html',
     audience: 'platform_api',
     scope: 'openid offline_access email transact'
   });
